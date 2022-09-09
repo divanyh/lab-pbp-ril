4d43706dcee78cadc0540ca8c4b15bf378cf6a74
Divany <harryndira@gmail.com>
2022-09-09 20:52:54 +0700

    Modify the templates

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,6 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_catalogue
+
+app_name = 'katalog'
+
+urlpatterns = [ path('', show_catalogue, name='show_catalogue')]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalogue(request):
+    isi_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': isi_katalog,
+        'nama': 'Divany Harryndira',
+        'npm': '2106701734'
+    }
+    return render(request, "katalog.html", context)
