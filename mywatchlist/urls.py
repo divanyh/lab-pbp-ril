6ca6be245f21ad40ff9de3a4557e59e636b2c317
Divany <harryndira@gmail.com>
2022-09-19 16:04:21 +0700

    Adding json view

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from mywatchlist.views import show_watchlist
+from mywatchlist.views import export_to_json
 
 app_name = 'mywatchlist'
 
-urlpatterns = [path('', show_watchlist, name='show_watchlist')]
+urlpatterns = [
+    path('', show_watchlist, name='show_watchlist'),
+    path('json/', export_to_json, name = 'export_to_json'),
+]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mywatchlist.models import WatchlistItem
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_watchlist(request):
@@ -10,3 +12,7 @@ def show_watchlist(request):
         'npm': '2106701734'
     }
     return render(request, "mywatchlist.html", context)
+
+def export_to_json(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type = "application/json")
