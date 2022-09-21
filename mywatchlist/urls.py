88de7edc0c47ee84c3c7c26efc4c1b9636f55668
Divany <harryndira@gmail.com>
2022-09-21 12:48:02 +0700

    Update

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from mywatchlist.views import show_watchlist
 from mywatchlist.views import export_to_json
+from mywatchlist.views import export_to_xml
 
 app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_watchlist, name='show_watchlist'),
+    path('html/', show_watchlist, name='show_watchlist'),
     path('json/', export_to_json, name = 'export_to_json'),
+    path('xml/', export_to_xml, name = 'export_to_xml'),
 ]
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,7 +1,11 @@
+from cgitb import html
+import imp
+from re import template
 from django.shortcuts import render
 from mywatchlist.models import WatchlistItem
 from django.http import HttpResponse
 from django.core import serializers
+from django.template import loader
 
 # Create your views here.
 def show_watchlist(request):
@@ -16,3 +20,7 @@ def show_watchlist(request):
 def export_to_json(request):
     data = WatchlistItem.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type = "application/json")
+
+def export_to_xml(request):
+    data = WatchlistItem.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type = "application/xml")
